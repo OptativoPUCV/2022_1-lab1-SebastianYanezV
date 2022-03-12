f091300cb600d485333006d84191ee312c91aca8
Your Name <you@example.com>
2022-03-12 18:00:25 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -141,6 +141,10 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
    Vector *vectorA = crearVector(largoVectorA);
    Vector *vectorB = crearVector(largoVectorB);
 
-   sumaV(vectorA, vectorB, c);
+   asignarValor(vectorA, 0, a1);
+   asignarValor(vectorA, 1, a2);
+   asignarValor(vectorB, 0, b1);
+   asignarValor(vectorB, 1, b2);
 
+   sumaV(vectorA, vectorB, c);
 }
